57a95ca8595f29d7f394f80bd55699aba3c38d2d
AP-Solution <haunt.beta@gmail.com>
2021-12-08 20:37:22 +0200

    new

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,10 @@
 window.addEventListener('DOMContentLoaded', ()=> {
 	const header = document.querySelector('header');
-	const hamburger = document.querySelector('.hamburger');
 	const asideMenu = document.querySelector('aside.menu');
-	const supportIcon = document.querySelector('div.support_icon');
 	const asideSupport = document.querySelector('aside.support');
-	const welcomeH2 = document.querySelector('h2.welcome_h2');
-	const welcomeH3 = document.querySelector('h3.welcome_h3');
-
-
-	setTimeout(() => {
-		welcomeH2.style.opacity = 1;
-		setTimeout(() => {
-			welcomeH3.style.opacity = 1;
-		}, 1000);
-	}, 1000);
 	window.addEventListener('scroll', () => {
 		if(pageYOffset != 0) {
 			header.style.backdropFilter = 'blur(5px)';
-			localStorage.setItem('KEY', window.pageYOffset);
 		} else {
 			header.style.backdropFilter = 'blur(0px)'
 		}
